6ec8ab8d82a7ef5d22a8d385d6d58aa44f9dd8a6
atoiyu123 <bothbboy@gmail.com>
2020-04-14 22:46:06 +0700

    add order.php and update coffee.php input buyname and number

--- coffee.php
+++ coffee.php
@@ -10,3 +10,11 @@ if(! $conn ){
 	die('Could not connect: ' . mysqli_error());
 }
 ?>
+
+<h1>สั่งเครื่องดื่ม</h1>
+    <form action="order.php" method="post">
+        <input type="text" name="buyname" id="buyname" placeholder="เครื่องดื่ม" style="width:350px;" />
+         
+        <input type="text" name="number" id="number" placeholder="จำนวนเครื่องดื่ม" style="width:350px;" />
+
+    </form>

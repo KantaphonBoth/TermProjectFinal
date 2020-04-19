13bf3f93fb47d64f90b6a798e75733fcfffacff3
atoiyu123 <bothbboy@gmail.com>
2020-04-19 14:36:53 +0700

    update index.php and fetch.php

--- fetch.php
+++ fetch.php
@@ -2,33 +2,33 @@
 
 //fetch.php
 
-$connect = new PDO("mysq:host=localhost;dbname=sales", "root", "Pang0808!!");
+$connect = new PDO("mysql:host=localhost;dbname=project", "root", "123456789");
 
-$column = array('CustomerID', 'ProductID', 'SaleDateTime', 'GrandTotal');
+$column = array('CustomerID', 'StaffID', 'GrandTotal', 'SaleDateTime');
 
 $query = '
-SELECT * FROM project
-WHERE CustomerID LIKE "%'.$_POST["search"]["seles"].'%" 
-OR ProductID LIKE "%'.$_POST["search"]["seles"].'%"
-OR SaleDateTime LIKE "%'.$_POST["search"]["seles"].'%"
-OR GrandTotal LIKE "%'.$_POST["search"]["seles"].'%"
+SELECT * FROM sales 
+WHERE CustomerID LIKE "%'.$_POST["search"]["value"].'%" 
+OR StaffID LIKE "%'.$_POST["search"]["value"].'%" 
+OR GrandTotal LIKE "%'.$_POST["search"]["value"].'%" 
+OR SaleDateTime LIKE "%'.$_POST["search"]["value"].'%" 
 
 ';
 
-if(isset($_POST["sales"]))
+if(isset($_POST["order"]))
 {
-    $query .= 'ORDER BY '.$column[$_POST['sales']['0']['column']].' '.$_POST['sales']['0']['dir']. ' ';
+ $query .= 'ORDER BY '.$column[$_POST['order']['0']['column']].' '.$_POST['order']['0']['dir'].' ';
 }
 else
 {
-    $query .= 'ORDER BY SaleID DESC ';
+ $query .= 'ORDER BY SaleID DESC ';
 }
 
 $query1 = '';
 
-if($_POST['length'] != -1)
+if($_POST["length"] != -1)
 {
-    $query1 = 'LIMIT ' . . $_POST['start'] . ', ' . $_POST['length'];
+ $query1 = 'LIMIT ' . $_POST['start'] . ', ' . $_POST['length'];
 }
 
 $statement = $connect->prepare($query);
@@ -41,7 +41,7 @@ $statement = $connect->prepare($query . $query1);
 
 $statement->execute();
 
-$result = $statement->fatchAll();
+$result = $statement->fetchAll();
 
 $data = array();
 
@@ -49,34 +49,33 @@ $total_order = 0;
 
 foreach($result as $row)
 {
-    $sub_array();
-    $sub_array[] = $row["CustomerID"];
-    $sub_array[] = $row["ProductID"];
-    $sub_array[] = $row["SaleDateTime"];
-    $sub_array[] = $row["GrandTotal"];
+ $sub_array = array();
+ $sub_array[] = $row["CustomerID"];
+ $sub_array[] = $row["StaffID"];
+ $sub_array[] = $row["GrandTotal"];
+ $sub_array[] = $row["SaleDateTime"];
 
-    $total_sales = $total_sales + floatval($row["GrandTotal"]);
-    $data[] =$sub_array
+ $total_order = $total_order + floatval($row["GrandTotal"]);
+ $data[] = $sub_array;
 }
 
 function count_all_data($connect)
 {
-    $query = "SELECT * FROM sales";
-    $statement = $connect->prepare($query);
-    $statement->execute();
-    return $statement->rowCount();
+ $query = "SELECT * FROM sales";
+ $statement = $connect->prepare($query);
+ $statement->execute();
+ return $statement->rowCount();
 }
 
 $output = array(
-    'draw'            =>  intval($_POST["draw"]),
-    'recordsTotal'    =>  count_all_data($connect),
-    'recordsFiltered' =>  $number_filter_row,
-    'data'            =>  $data,
-    'total'           => number_format($total_order, 2)
+ 'draw'    => intval($_POST["draw"]),
+ 'recordsTotal'  => count_all_data($connect),
+ 'recordsFiltered' => $number_filter_row,
+ 'data'    => $data,
+ 'total'    => number_format($total_order, 2)
 );
 
 echo json_encode($output);
-
 
 
 ?>

38a04f847d2afa07d9e971e5395fa698e6e41910
atoiyu123 <bothbboy@gmail.com>
2020-04-25 01:43:19 +0700

    update

--- index.php
+++ index.php
@@ -3,15 +3,32 @@
  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
   <script src="https://cdn.datatables.net/1.10.12/js/jquery.dataTables.min.js"></script>
- <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>  
+ <!-- <script src="https://cdn.datatables.net/1.10.12/js/dataTables.bootstrap.min.js"></script>   -->
  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.12/css/dataTables.bootstrap.min.css" />
  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
  <script src="https://www.jqueryscript.net/demo/Dialog-Modal-Dialogify/dist/dialogify.min.js"></script> 
+ <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Chonburi&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display&display=swap" rel="stylesheet">
+	<!-- Bootstrap css -->
+	<link href="css/bootstrap.css" rel="stylesheet" media="screen">
+	<!-- Font Awesome Icon font css -->
+	<link href="css/fontawesome-all.css" rel="stylesheet">
+	<!-- Flaticon Icon font css -->
+	<link href="css/flaticon.css" rel="stylesheet">
+	<!-- Swiper's CSS -->
+	<link rel="stylesheet" href="css/swiper.min.css">
+	<!-- SlickNav Menu css -->
+	<link href="css/slicknav.css" rel="stylesheet" media="screen">
+	<!-- Main custom css -->
+	<link href="css/custom.css" rel="stylesheet" media="screen">
+    <link href="css/responsive.css" rel="stylesheet" media="screen">
+
 </head>
  <body>
  <meta http-equiv="Content-Type" content="Text/html; charset=tis-620">
  <title></title>
- <style type="text/css">
+ <!-- <style type="text/css">
  @font-face {
         font-family :Bangnampueng;
         src :url('Bangnampueng.ttf')format("truetype");
@@ -60,13 +77,114 @@
       color: black;
   }
 
-  </style>
+  </style> -->
 
 <div class="preloader">
 		<div class="loader"></div>
       </div>
-      
-      
+	<!-- Preloader Ends -->
+	
+	<!-- Header Section Starts -->
+	<header class="header-default">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="logo">
+						<a class="navbar-brand" href="#">
+							<img src="images/logo.png" alt="" />
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+	<!-- Header Section ends-->
+	
+	<!-- Navigation Starts -->
+	<div class="main-navigation" id="main-navbar">
+		<div class="menu-wrapper">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<nav class="navbar navbar-expand-md navbar-light" id="navigation">
+							<ul class="navbar-nav mx-auto" id="main-menu">
+								<li class="nav-item"><a class="nav-link active" href="#home">Home</a></li>
+								<li class="nav-item"><a class="nav-link active" href="#menu">Menu</a></li>
+								<li class="nav-item"><a class="nav-link " href="service.php">Service</a></li>
+								<li class="nav-item"><a class="nav-link" href="myorder.php">My Oder</a></li>
+							
+							</ul>	
+							<div class="navbar-toggle"><span>Menu</span></div>
+							<div id="responsive-menu"></div>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</div>
+</div>			
+							
+								
+							
+
+	<!-- Navigation Ends -->
+	
+	<!-- Home Slider Section Starts -->
+	<div class="home-slider-section" id="home">
+		<div class="container-fluid">
+			<div class="row no-gutter">
+				<div class="col-md-12">
+					<div class="swiper-container home-slider">
+						<div class="swiper-wrapper">
+							<div class="swiper-slide">
+								<div class="home-slide" style="background-image: url(images/55.jpg);">
+									<div class="home-slide-content">
+										<h3>Hello Everyone!</h3>
+										<h2>This is the COFFEE Shop, It's online website, If you find coffee shop online
+										<br />
+									that right to be here!</h2>
+									</div>
+								</div>
+							</div>
+							
+							<div class="swiper-slide">
+								<div class="home-slide" style="background-image: url(images/56.jpg);">
+									<div class="home-slide-content">
+										<h3>Hello Everyone!</h3>
+										<h2>This is the COFFEE Shop, It's online website, If you find coffee shop online
+										<br />
+									that right to be here!</h2>
+									</div>
+								</div>
+							</div>
+							<div class="swiper-slide">
+								<div class="home-slide" style="background-image: url(images/57.jpg);">
+									<div class="home-slide-content">
+										<h3>Hello Everyone!</h3>
+										<h2>This is the COFFEE Shop, It's online website, If you find coffee shop online
+										<br />
+									that right to be here!</h2>
+									</div>
+								</div>
+							</div>
+							<div class="swiper-slide">
+								<div class="home-slide" style="background-image: url(images/59.jpg);">
+									<div class="home-slide-content">
+										<h3>Hello Everyone!</h3>
+										<h2>This is the COFFEE Shop, It's online website, If you find coffee shop online
+										<br />
+									that right to be here!</h2>
+									</div>
+								</div>
+							</div>
+						</div>
+						
+						<div class="home-slider-prev"><i class="fa fa-angle-left"></i></div>
+						<div class="home-slider-next"><i class="fa fa-angle-right"></i></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
   <div class="container box">
    <h3>SALES SUMMARY</h3>
    <br />
@@ -95,7 +213,50 @@
    </div>
   </div>
  
-  <script src="function.js"></script>
+  <!-- <script src="js/jquery-1.12.4.min.js"></script> -->
+	<!-- Bootstrap js file -->
+	<!-- <script src="js/bootstrap.min.js"></script> -->
+	<!-- Swiper Carousel js file -->
+	<script src="js/swiper.min.js"></script>
+	<!-- SlickNav js file -->
+	<script src="js/jquery.slicknav.js"></script>
+	<!-- Smooth Scroll js file -->
+	<script src="js/SmoothScroll.js"></script>
+    <script src="js/function.js"></script>
+<script type="text/javascript" language="javascript" >
+ $(document).ready(function(){
+  
+   var dataTable = $('#order_data').DataTable({
+    "processing" : true,
+    "serverSide" : true,
+    "order" : [],
+    "ajax" : {
+     url:"fetch.php",
+     type:"POST"
+    },
+    drawCallback:function(settings)
+    {
+     $('#total_order').html(settings.json.total);
+    }
+   });
+    
+   $(document).on('click', '.view', function(){
+  var id = $(this).attr('id');
+  var options = {
+   ajaxPrefix: '',
+   ajaxData: {id:id},
+   ajaxComplete:function(){
+    this.buttons([{
+     type: Dialogify.BUTTON_PRIMARY
+    }]);
+   }
+  };
+  new Dialogify('fetch_single.php', options)
+   .title('View Employee Details')
+   .showModal();
+ });
 
-</body>
+ });
+ 
+</script></body>
 </html>

90cea1c7a78be9f90945ce470117824689918909
thidarat2542 <thidarat15care@gmail.com>
2020-04-29 04:25:46 +0700

    update order.php

--- order.php
+++ order.php
@@ -85,7 +85,17 @@
             <?php } ?>
             
     </div>
+    <?php
 
+if(isset($_POST['logout']))
+{
+    session_start();
+    session_unset();
+    session_destroy();
+    header("location: coffee.php");
+    exit();
+}
+?>
 
 
     <div class="clearfix"></div>
